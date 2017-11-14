76576785ef3127aa845aaf261bcc9a4179c91bb9
Romain TRUCHI <r.truchi@wcie.fr>
2017-11-14 17:36:52 +0100

    WebPack! :]

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js'
+, devtool: 'inline-source-map'
+, devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  , host: '0.0.0.0'
+  , port: 9000
+  , compress: true
+  , noInfo: true
+  , clientLogLevel: 'none'
+  , overlay: true
+  , useLocalIp: true
+  }
+, output: {
+    filename: 'app.js'
+  , path: path.resolve(__dirname, 'dist')
+  }
+}
